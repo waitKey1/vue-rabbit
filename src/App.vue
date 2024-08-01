3673a69b8da8e2a80eaeb700a6aacdf0d9694582
waitKey1 <waitKey1@outlook.com>
2024-08-01 11:05:59 +0800

    路由设计

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@ import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
-      <el-button type="primary">Primary</el-button>
+
+      <RouterView />
 </template>
 
 <style scoped>

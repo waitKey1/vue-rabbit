d82848c55ad42d956e2804d2d7bad9d11ca9386e
waitKey1 <waitKey1@outlook.com>
2024-08-01 11:13:01 +0800

    静态资源引入和插件安装

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+//引入初始化样式文件
+import '@/styles/common.scss'
 
 //测试接口函数
 import { getCategory } from './apis/testAPI'
@@ -12,6 +14,7 @@ getCategory().then(res =>{
   console.log(res)
 })
 const app = createApp(App)
+
 
 app.use(createPinia())
 app.use(router)

42554e54081b51d5216c88cbccbf518e9791c159
waitKey1 <waitKey1@outlook.com>
2024-08-01 10:45:38 +0800

    axios 封装

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,12 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+     
+      resolvers: [
+         //1.配置elementplus 才能sass样式配色系统
+        ElementPlusResolver({importStyle:'sass'})
+      ],
+
     }),
   ],
   resolve: {
@@ -23,5 +28,16 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  css:{
+    preprocessorOptions:{
+      scss:{
+        //2.自动导入定制化样式文件进行样式覆盖
+        additionalData:`
+        @use "@/styles/element/index.scss" as *;
+        `,
+      }
+    }
+   
   }
 })
